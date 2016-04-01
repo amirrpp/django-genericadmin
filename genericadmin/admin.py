7b9cee36ecbc4d516617a0e06bb76df6f2d3fd05
amirrpp <amirrpp@yandex.ru>
2016-04-01 16:16:06 +0300

    only for django 1.9

--- genericadmin/admin.py
+++ genericadmin/admin.py
@@ -70,9 +70,9 @@ class BaseGenericModelAdmin(object):
                     })
                     
         if hasattr(self, 'inlines') and len(self.inlines) > 0:
-            for FormSet, inline in zip(self.get_formsets(request), self.get_inline_instances(request)):
+            for FormSet, inline in zip(self.get_formsets_with_inlines(request), self.get_inline_instances(request)):
                 if hasattr(inline, 'get_generic_field_list'):
-                    prefix = FormSet.get_default_prefix()
+                    prefix = FormSet[0].get_default_prefix()
                     field_list = field_list + inline.get_generic_field_list(request, prefix)
 
         return field_list
